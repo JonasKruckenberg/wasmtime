58610875771c4ba0f6a69d0044c4be9bea549c88
Jonas Kruckenberg <iterpre@protonmail.com>
2024-09-09 11:04:56 +0200

    refactor: mark `cranelift-codegen-shared` as no_std

--- cranelift/codegen/shared/src/lib.rs
+++ cranelift/codegen/shared/src/lib.rs
@@ -1,6 +1,7 @@
 //! This library contains code that is common to both the `cranelift-codegen` and
 //! `cranelift-codegen-meta` libraries.
 
+#![no_std]
 #![deny(missing_docs)]
 
 pub mod constant_hash;
