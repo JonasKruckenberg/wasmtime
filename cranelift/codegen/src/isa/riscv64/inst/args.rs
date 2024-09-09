3928720e36251c9202c8426bf2be1a8c4a8422b9
Jonas Kruckenberg <iterpre@protonmail.com>
2024-09-09 12:21:29 +0200

    refactor: use `libm` for no_std float immediate methods

--- cranelift/codegen/src/isa/riscv64/inst/args.rs
+++ cranelift/codegen/src/isa/riscv64/inst/args.rs
@@ -2,12 +2,10 @@
 
 use super::*;
 use crate::ir::condcodes::CondCode;
-
 use crate::isa::riscv64::lower::isle::generated_code::{
     COpcodeSpace, CaOp, CbOp, CiOp, CiwOp, ClOp, CrOp, CsOp, CssOp, CsznOp, FpuOPWidth, ZcbMemOp,
 };
 use crate::machinst::isle::WritableReg;
-
 use core::fmt::Result;
 
 /// A macro for defining a newtype of `Reg` that enforces some invariant about
@@ -342,10 +340,24 @@ impl FliConstant {
             (F32, f) if f == (f32::MIN_POSITIVE as f64) => Self::new(1),
             (F64, f) if f == f64::MIN_POSITIVE => Self::new(1),
 
+            #[cfg(feature = "std")]
             (_, f) if f == 2.0f64.powi(-16) => Self::new(2),
+            #[cfg(feature = "std")]
             (_, f) if f == 2.0f64.powi(-15) => Self::new(3),
+            #[cfg(feature = "std")]
             (_, f) if f == 2.0f64.powi(-8) => Self::new(4),
+            #[cfg(feature = "std")]
             (_, f) if f == 2.0f64.powi(-7) => Self::new(5),
+            
+            #[cfg(feature = "core")]
+            (_, f) if f == libm::pow(2.0, -16.0) => Self::new(2),
+            #[cfg(feature = "core")]
+            (_, f) if f == libm::pow(2.0, -15.0) => Self::new(3),
+            #[cfg(feature = "core")]
+            (_, f) if f == libm::pow(2.0, -8.0) => Self::new(4),
+            #[cfg(feature = "core")]
+            (_, f) if f == libm::pow(2.0, -7.0) => Self::new(5),
+            
             (_, f) if f == 0.0625 => Self::new(6),
             (_, f) if f == 0.125 => Self::new(7),
             (_, f) if f == 0.25 => Self::new(8),
